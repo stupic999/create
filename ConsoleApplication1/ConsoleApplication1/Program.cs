10deb7c3d35d42ba0e9af97cd06e9ac7d4f036c1
DESKTOP-H468MRI\Moucy Cat <aierzhiguang2@gmail.com>
2018-03-31 12:16:36 +0800

    可以製造大量文件夾，想做多少只要改變i的數值即可

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i > 0; i-=7)
+            {
+                Directory.CreateDirectory("try"+i); 
+            }
+        }
+    }
+}
